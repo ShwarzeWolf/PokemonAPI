ebff88e357d9e3281288c1a495ea0e15975c8350
Kristina Sharaeva <224787@niuitmo.ru>
2022-06-16 18:34:08 +0300

    Getiing pokemon from generations implemented
     - Function _get_genarations implemented to get all generation species from generations into csv file
    and load them locally
    
     -  Function _get_generations_species implemented to get all pokemons from generations_species

--- main.py
+++ main.py
@@ -45,3 +45,41 @@ def _check_generations_count():
     generations = requests.get(url).json()
 
     logging.info(f'Today exist {generations["count"]} generations')
+
+
+def _get_generations():
+    """Gets list of generation from pokemon API and logs info about generation count
+       Will migrate _to get_generations task in DAG"""
+    url = 'https://pokeapi.co/api/v2/generation'
+    generations = requests.get(url).json()['results']
+
+    pokemon_generations_chunks = []
+
+    for generation in generations:
+        url = generation['url']
+        response = requests.get(url).json()['pokemon_species']
+        pokemon_species = pd.json_normalize(response)
+
+        pokemon_species['generation'] = generation['name']
+        pokemon_species['id'] = pokemon_species.apply(lambda row: row['url'].split('/')[-2], axis=1)
+        pokemon_generations_chunks.append(pokemon_species)
+
+    pokemon_generations = pd.concat(pokemon_generations_chunks)
+    pokemon_generations.to_csv('./pokemon_generations.csv', index=False, header=True)
+
+
+def _get_generations_species():
+    """Gets all pokemons from pokemon species"""
+    url = 'pokemon_generations.csv'
+    pokemon_species = pd.read_csv(url)['url'].unique()
+
+    species_chunks = []
+    for specie in pokemon_species:
+        response = requests.get(specie).json()['varieties']
+        pokemon_species = pd.json_normalize(response)
+        pokemon_species['specie_id'] = specie.split('/')[-2]
+        species_chunks.append(pokemon_species)
+
+    pokemon_species = pd.concat(species_chunks)
+    pokemon_species.to_csv('./species_pokemons.csv', index=False, header=True)
+
