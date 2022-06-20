5f6c284bf5bf9f44977a5fe331268f2c0dedd6f3
Kristina Sharaeva <224787@niuitmo.ru>
2022-06-20 21:13:19 +0300

    Refactoring: beautifying and setting all code style to common one

--- sharaeva_functions/fetch_api_functions.py
+++ sharaeva_functions/fetch_api_functions.py
@@ -12,29 +12,32 @@ def _get_pokemons():
 
     url = f'https://pokeapi.co/api/v2/pokemon?offset=0&limit={pokemons_count}'
     pokemons_raw = get_data_by_url(url)['results']
-
     pokemons = pd.json_normalize(pokemons_raw)
-    save_file_into_S3('pokemons.csv', pokemons)
+
+    save_file_into_S3(filename='pokemons.csv', data=pokemons)
 
 
 def _get_pokemons_stats():
     """Gets list of pokemons from API and loads the into pokemon_stats file
     Will move to _get_pokemons dag in Airflow"""
-    pokemons = read_file_from_S3('pokemons.csv')
+    pokemons = read_file_from_S3(filename='pokemons.csv')
 
-    pokemon_stats_chunks = []
+    pokemons_stats_chunks = []
 
     for index, pokemon in pokemons.iterrows():
         url = pokemon['url']
         raw_pokemon_stats = get_data_by_url(url)['stats']
 
-        pokemon_stats_chunk = pd.json_normalize(raw_pokemon_stats) \
-                                .rename(columns={'base_stat': 'value'})
-        pokemon_stats_chunk['pokemon'] = pokemon['name']
+        pokemons_stats_chunk = pd.json_normalize(raw_pokemon_stats)
 
-        pokemon_stats_chunks.append(pokemon_stats_chunk)
+        if not pokemons_stats_chunk.empty:
+            pokemons_stats_chunk['pokemon'] = pokemon['name']
+            pokemons_stats_chunk.rename(columns={'base_stat': 'power', 'stat.name': 'stat'})
+            pokemons_stats_chunk.drop(columns=['effort', 'stat.url'], inplace=True)
 
-    pokemons_stats = pd.concat(pokemon_stats_chunks)
+            pokemons_stats_chunks.append(pokemons_stats_chunk)
+
+    pokemons_stats = pd.concat(pokemons_stats_chunks)
     save_file_into_S3(filename='pokemons_stats.csv', data=pokemons_stats)
 
 
@@ -45,15 +48,15 @@ def _get_types():
 
     url = f'https://pokeapi.co/api/v2/type?offset=0&limit={types_count}'
     types_raw = get_data_by_url(url)['results']
-
     types = pd.json_normalize(types_raw)
-    save_file_into_S3('types.csv', types)
+
+    save_file_into_S3(filename='types.csv', data=types)
 
 
 def _get_pokemon_types():
     """Loads all types and pokemons from Pokemon API into pokemon_types CSV file.
        Will migrate to get_types task in DAG"""
-    types = read_file_from_S3('types.csv')
+    types = read_file_from_S3(filename='types.csv')
 
     pokemons_types_chunks = []
 
@@ -61,22 +64,18 @@ def _get_pokemon_types():
         url = _type['url']
 
         response = get_data_by_url(url)['pokemon']
-        pokemons_type = pd.json_normalize(response)
+        pokemons_types_chunk = pd.json_normalize(response)
 
-        if not pokemons_type.empty:
-            pokemons_type['type'] = _type['name']
+        if not pokemons_types_chunk.empty:
+            pokemons_types_chunk['type'] = _type['name']
 
-            # TODO на подумать - так ли нам нужен id или name в этой таблице?
-            #  Там же функциональная зависимость между ними
-            pokemons_type['id'] = pokemons_type.apply(lambda row: row['pokemon.url'].split('/')[-2], axis=1)
-            pokemons_type.rename(columns={'pokemon.name': 'name'}, inplace=True)
-            pokemons_type.drop(columns=['slot', 'pokemon.url'], inplace=True)
+            pokemons_types_chunk.rename(columns={'pokemon.name': 'pokemon'}, inplace=True)
+            pokemons_types_chunk.drop(columns=['slot', 'pokemon.url'], inplace=True)
 
-            pokemons_types_chunks.append(pokemons_type)
+            pokemons_types_chunks.append(pokemons_types_chunk)
 
     pokemons_types = pd.concat(pokemons_types_chunks)
-    save_file_into_S3('pokemons_types.csv', pokemons_types)
-
+    save_file_into_S3(filename='pokemons_types.csv', data=pokemons_types)
 
 
 def _get_generations():
@@ -86,45 +85,52 @@ def _get_generations():
 
     url = f'https://pokeapi.co/api/v2/generation?offset=0&limit={generations_count}'
     generations_raw = get_data_by_url(url)['results']
-
     generations = pd.json_normalize(generations_raw)
-    save_file_into_S3('generations.csv', generations)
+
+    save_file_into_S3(filename='generations.csv', data=generations)
 
 
 def _get_generations_species():
     """Gets list of generation from pokemon API and logs info about generation count
        Will migrate _to get_generations task in DAG"""
-    generations = read_file_from_S3('generations.csv')
+    generations = read_file_from_S3(filename='generations.csv')
 
-    pokemon_generations_chunks = []
+    pokemons_generations_chunks = []
 
     for index, generation in generations.iterrows():
         url = generation['url']
         response = get_data_by_url(url)['pokemon_species']
-        pokemon_species = pd.json_normalize(response)
+        pokemons_species_chunk = pd.json_normalize(response)
 
-        pokemon_species['generation'] = generation['name']
-        pokemon_species['id'] = pokemon_species.apply(lambda row: row['url'].split('/')[-2], axis=1)
-        pokemon_generations_chunks.append(pokemon_species)
+        if not pokemons_species_chunk.empty:
+            pokemons_species_chunk['generation'] = generation['name']
+            pokemons_species_chunk['specie_id'] = pokemons_species_chunk.apply(lambda row: row['url'].split('/')[-2], axis=1)
+            pokemons_species_chunk.rename(columns={'name': 'specie_name', 'url': 'specie_url'}, inplace=True)
 
-    generations_species = pd.concat(pokemon_generations_chunks)
-    save_file_into_S3('generations_species.csv', generations_species)
+            pokemons_generations_chunks.append(pokemons_species_chunk)
+
+    generations_species = pd.concat(pokemons_generations_chunks)
+    save_file_into_S3(filename='generations_species.csv', data=generations_species)
 
 
 def _get_pokemons_species():
     """Gets all pokemons from pokemon species"""
-    pokemon_species = read_file_from_S3('generations_species.csv')['url'].unique()
+    pokemons_species_chunk = read_file_from_S3(filename='generations_species.csv')['specie_url'].unique()
 
-    species_chunks = []
+    pokemons_species_chunks = []
 
-    for specie in pokemon_species:
+    for specie in pokemons_species_chunk:
         response = get_data_by_url(specie)['varieties']
-        pokemon_species = pd.json_normalize(response)
-        pokemon_species['specie_id'] = specie.split('/')[-2]
-        species_chunks.append(pokemon_species)
+        pokemons_species_chunk = pd.json_normalize(response)
 
-    pokemon_species = pd.concat(species_chunks)
-    save_file_into_S3('pokemons_species.csv', pokemon_species)
+        pokemons_species_chunk['specie_id'] = specie.split('/')[-2]
+        pokemons_species_chunk.rename(columns={'pokemon.name': 'pokemon'}, inplace=True)
+        pokemons_species_chunk.drop(columns=['is_default', 'pokemon.url'], inplace=True)
+
+        pokemons_species_chunks.append(pokemons_species_chunk)
+
+    pokemons_species = pd.concat(pokemons_species_chunks)
+    save_file_into_S3(filename='pokemons_species.csv', data=pokemons_species)
 
 
 def _get_moves():
@@ -134,31 +140,32 @@ def _get_moves():
 
     url = f'https://pokeapi.co/api/v2/move?offset=0&limit={moves_count}'
     moves_raw = get_data_by_url(url)['results']
-
     moves = pd.json_normalize(moves_raw)
-    save_file_into_S3('moves.csv', moves)
+
+    save_file_into_S3(filename='moves.csv', data=moves)
 
 
 def _get_pokemon_moves():
     """Gets all moves and pokemons from pokemon API"""
-    moves = read_file_from_S3('moves.csv')
+    moves = read_file_from_S3(filename='moves.csv')
 
-    pokemon_moves_chunks = []
+    pokemons_moves_chunks = []
 
     for index, move in moves.iterrows():
         url = move['url']
         response = get_data_by_url(url)['learned_by_pokemon']
 
-        pokemon_moves_chunk = pd.json_normalize(response)
+        pokemons_moves_chunk = pd.json_normalize(response)
 
-        if not pokemon_moves_chunk.empty:
-            pokemon_moves_chunk['move'] = move['name']
-            pokemon_moves_chunk['pokemon_id'] = pokemon_moves_chunk.apply(lambda row: row['url'].split('/')[-2], axis=1)
-            pokemon_moves_chunk.rename(columns={'name': 'pokemon_name'}, inplace=True)
-            pokemon_moves_chunks.append(pokemon_moves_chunk)
+        if not pokemons_moves_chunk.empty:
+            pokemons_moves_chunk['move'] = move['name']
+            pokemons_moves_chunk.rename(columns={'name': 'pokemon'}, inplace=True)
+            pokemons_moves_chunk.drop(columns=['url'], inplace=True)
 
-    pokemon_moves = pd.concat(pokemon_moves_chunks)
-    save_file_into_S3('pokemons_moves.csv', pokemon_moves)
+            pokemons_moves_chunks.append(pokemons_moves_chunk)
+
+    pokemon_moves = pd.concat(pokemons_moves_chunks)
+    save_file_into_S3(filename='pokemons_moves.csv', data=pokemon_moves)
 
 
 def _check_generations_count():
